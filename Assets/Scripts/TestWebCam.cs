abf3a3ad3366b8c1ac7db14098b323b2a9cc10f2
Nicolas Hognon <nicolas.hognon@gmail.com>
2016-01-30 00:51:03 +0100

    TimedInputSequence. TestWebCam

---
+++ Assets/Scripts/TestWebCam.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestWebCam : MonoBehaviour {
+
+    public Renderer renderer;
+    
+    private string _deviceName;
+    
+	// Use this for initialization
+	void Start () {
+        foreach(var wcam in WebCamTexture.devices) {
+            _deviceName = wcam.name;
+            WebCamTexture webcam = new WebCamTexture(_deviceName);
+            renderer.material.mainTexture = webcam;
+            webcam.Play();	
+            break;            
+        }
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
