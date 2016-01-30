38ad3bc88f786b914058acdb1c9da0f8e4f8b140
Nicolas Hognon <nicolas.hognon@gmail.com>
2016-01-30 16:55:31 +0100

    KeyboardSlide

---
+++ Assets/Scripts/KeyboardSlide.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyboardSlide : MonoBehaviour {
+
+    public GameManager manager;
+    public KeyCode startLetter;
+    public KeyCode endLetter;
+    public int letterCount = 3;
+    public float timeout = 5;
+    public GameManager.SuccessType success;
+
+    private float _startTime = 0;
+    private string _letters;
+    
+    void OnEnable() {
+       _startTime = Time.time;
+       _letters = "";
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	   if(Input.anyKeyDown) {
+           _letters += Input.inputString.ToUpper();
+           Debug.Log(Input.inputString);
+           Debug.Log(startLetter.ToString().ToUpper());
+           if ( _letters[0].ToString()==startLetter.ToString() && 
+                _letters[_letters.Length-1].ToString()==endLetter.ToString() && 
+                _letters.Length>letterCount) {
+                Debug.Log("OK: " + _letters);
+                gameObject.SetActive(false);
+                manager.Success(success);
+           }
+       }
+
+       if ((Time.time - _startTime) > timeout) {
+           Debug.Log("timeout FAILED (" + (Time.time - _startTime) + ")");
+           manager.Failed(GameManager.FailType.TooLong);
+           gameObject.SetActive(false);
+       }
+	}
+}
