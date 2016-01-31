65449fbedd6a69186fa5758c48fb300ddebba191
Nicolas Hognon <nicolas.hognon@gmail.com>
2016-01-31 09:51:32 +0100

    add MeshSortingLayer

---
+++ Assets/Scripts/MeshSortingLayer.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MeshSortingLayer : MonoBehaviour {
+
+	public string layerName = "Default";
+
+	void Awake()
+	{
+		GetComponent<Renderer>().sortingLayerName = layerName;
+	}
+
+}
